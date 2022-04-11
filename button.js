8f228e376f3bc2677fca02b5725349fa8798ec20
DragonAss-DEV <71887969+DragonASS-Dev@users.noreply.github.com>
2022-04-11 20:38:59 +0200

    V1.2
    Bug fix

---
+++ button.js
@@ -0,0 +1,5 @@
+document.getElementById('btn').addEventListener('click', function() {
+
+    window.open('https://freecodecamp.org/certification/dragonass-dev/scientific-computing-with-python-v7');
+  
+  });
