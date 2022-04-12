3bc7ac5e28ef56ea6a3320d1cc7f0c0cf03f0445
DragonAss-DEV <71887969+DragonASS-Dev@users.noreply.github.com>
2022-04-12 20:35:41 +0200

    V1.4
    Fixed link issue

--- button.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('btn').addEventListener('click', function() {
-
-    window.open('https://freecodecamp.org/certification/dragonass-dev/scientific-computing-with-python-v7');
-  
-  });
